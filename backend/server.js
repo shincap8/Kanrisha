9f6c618013718cc285e477649e4b28fb0844bbb1
pafuentess <pafuentess@unal.edu.co>
2020-05-30 14:51:39 -0500

    add cors use

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,13 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 // routes
 app.use(require('./routes/project'));
