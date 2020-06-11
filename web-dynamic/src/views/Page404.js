9b7016a0d73fe75f5a645edf781da940071893df
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-06-10 19:33:10 -0500

    View Page 404

---
+++ web-dynamic/src/views/Page404.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../Page404.css';
+import error from '../img/error.png';
+import Logo from '../img/Logo.png';
+
+export class Page404 extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                    <div className="col-md-6 content">
+                        <div className="text">
+                            <p>OOPS! </p>
+                            <p>We Couldn't Find That Page</p>
+                        </div>
+                        <div>
+                            <img className="imagen col-md-12 mt-2 ml-3" src={ error } alt="pc" />
+                        </div>
+                        <div className="text">
+                            <p>Go Home:</p>
+                            <a href="http://localhost:3000">
+                                <img className="logo" src={ Logo } alt="link" />
+                            </a>
+                        </div>
+                    </div>
+            </div>
+
+        );
+    }
+}
+
+export default Page404;
